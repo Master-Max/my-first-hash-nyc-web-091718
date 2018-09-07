04872713366b5b0c4b73a714058f008248c8fa2f
Max Petersen <mglxp5@gmail.com>
2018-09-07 22:01:25 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -15,7 +15,7 @@ def shipping_manifest
 
 end
 
-def retrieval(item)
+def retrieval
   shipping_manifest = {
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
@@ -23,7 +23,7 @@ def retrieval(item)
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-  shipping_manifest[item]
+  shipping_manifest["oil paintings"]
 end
 
 def adding
